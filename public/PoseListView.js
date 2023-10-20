3cdd6ce56eec0be443b337b48f6aaacd7c6bf026
Daniel Toll <ttoda@lnu.se>
2023-10-20 10:21:24 +0200

    pose titles

--- public/PoseListView.js
+++ public/PoseListView.js
@@ -8,20 +8,22 @@ class PoseListView {
         const htmlPoseList = document.getElementById("poseList");
         this.#poseList = poseList
 
-        let lastWasATitle
+        let lastPoseTitle
 
         this.#poseList.getPoses().forEach((pose, index) => {
             
             if (pose instanceof PoseTitle) {
                 const li = document.createElement("li");
                 li.innerHTML = `${pose.count} st ${pose.title} - ${pose.timeString()}`;
-                li.style.backgroundColor="#ffffff"
+                li.className = "ListPoseTitle"
                 htmlPoseList.appendChild(li);
+                lastPoseTitle = li
             } else {
                 const li = document.createElement("li");
                 li.innerHTML = `${pose.title} ${pose.timeString()}`;
                 li.id = `pose-${index}`;
                 li.pose = pose //we save the pose item here
+                li.lastPoseTitle = lastPoseTitle
                 li.addEventListener("click", () => setCurrentPose(pose));
                 htmlPoseList.appendChild(li);
             }
@@ -33,6 +35,10 @@ class PoseListView {
         const poseItems = document.querySelectorAll(".pose-list li");
         poseItems.forEach((item, index) => {
             item.classList.toggle("active", item.pose === this.#poseList.getActivePost());
+
+            if (item.pose === this.#poseList.getActivePost()) {
+                item.lastPoseTitle.scrollIntoView({ behavior: "smooth" });
+            }
         });
     }
     
